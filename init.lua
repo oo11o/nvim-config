15dac983dd0aaa0c5e9f8eef9ebda25dd8b00c4d
Volodymyr <ymyr.vlad@gmail.com>
2024-06-30 11:27:17 +0300

    init

---
+++ init.lua
@@ -0,0 +1,10 @@
+-- Enable line number
+vim.o.number = true
+
+-- Tab Settings
+vim.o.tabstop = 4 -- Set the number of spaces for tab stops
+vim.o.shiftwidth = 4 -- Set the number of spaces to use for each step of (auto)indent
+vim.o.expandtab = true -- Convert tabs to spaces
+
+-- Plugin settings using Packer
+-- vim.cmd [[packadd packer.nvim]]
